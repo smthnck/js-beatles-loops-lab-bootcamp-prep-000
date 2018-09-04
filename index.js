0d3d0235076b6b662602276fe64838d8a2d8a004
Nick Smith <smthnck@gmail.com>
2018-09-04 00:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+var theBeatlesPlay = function(a,b){
+  var arr = [];
+  for(var i=0;i<a.length;i++){
+    arr.push(`${a[i]} plays ${b[i]}`)
+  }
+  return arr
+}
+
+var johnLennonFacts = function(a){
+  var arr = [];
+  var j =0;
+  while (j < a.length){
+    arr.push(`${a[j]}!!!`)
+    j=j+1;
+  }
+  return arr
+}
+
+var iLoveTheBeatles = function(){}
